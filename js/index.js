e9d5fefebc6734bf58cd683eb7ed9b471823dfc6
Alex Bosch <abosch19@gmail.com>
2015-09-22 18:22:19 +0200

    NO COMMENTS

--- js/index.js
+++ js/index.js
@@ -8,28 +8,15 @@ var jugadors = [
 	"nom": "Alberto",
 	"dorsal": 5,
 	"punts": 2
-	},
-	{
-	"nom": "Jorge",
-	"dorsal": 3,
-	"punts": -4
-	},
-	{
-	"nom": "Jesus",
-	"dorsal": 6,
-	"punts": 1
-	}	
+	}
 ]
 
 
-$(document).ready(function () {
+$(document).ready({
 	
-	var comunio = jugadors.sort(function (a,b) {
-		return parseFloat(b.punts) - parseFloat(a.punts)
-		})
 	jugadors.forEach(function (data) {
-		$(".cabecera").append("<tr><td>" + data.dorsal + "</td><td>" + data.nom + "</td><td>"+ data.punts
- + "</td></tr>")
+		$("ol").append("<li>" + data.nom + "</li>" );
 	})
 
-});
+
+})
