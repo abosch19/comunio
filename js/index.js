2688e7803eae3e64f29b675ae2ac6964e9dca346
Alex Bosch <abosch19@gmail.com>
2015-09-22 18:09:48 +0200

    Taula creada, jquery append de jugadors, punts, dorsal

--- js/index.js
+++ js/index.js
@@ -1,4 +1,35 @@
-(function () {
+var jugadors = [
+	{
+	"nom": "Alex",
+	"dorsal": 11,
+	"punts": 0
+	},
+	{
+	"nom": "Alberto",
+	"dorsal": 5,
+	"punts": 2
+	},
+	{
+	"nom": "Jorge",
+	"dorsal": 3,
+	"punts": -4
+	},
+	{
+	"nom": "Jesus",
+	"dorsal": 6,
+	"punts": 1
+	}	
+]
 
- 
-})()
+
+$(document).ready(function () {
+	
+	var comunio = jugadors.sort(function (a,b) {
+		return parseFloat(b.punts) - parseFloat(a.punts)
+		})
+	jugadors.forEach(function (data) {
+		$(".cabecera").append("<tr><td>" + data.dorsal + "</td><td>" + data.nom + "</td><td>"+ data.punts
+ + "</td></tr>")
+	})
+
+});
