ebd864e4b6e5752029009d362afe43c3a86c922e
Alex Bosch <abosch19@gmail.com>
2015-09-22 19:40:05 +0200

    CORRECTE

--- js/index.js
+++ js/index.js
@@ -8,15 +8,28 @@ var jugadors = [
 	"nom": "Alberto",
 	"dorsal": 5,
 	"punts": 2
-	}
+	},
+	{
+	"nom": "Jorge",
+	"dorsal": 3,
+	"punts": -4
+	},
+	{
+	"nom": "Jesus",
+	"dorsal": 6,
+	"punts": 1
+	}	
 ]
 
 
-$(document).ready({
+$(document).ready(function () {
 	
+	var comunio = jugadors.sort(function (a,b) {
+		return parseFloat(b.punts) - parseFloat(a.punts)
+		})
 	jugadors.forEach(function (data) {
-		$("ol").append("<li>" + data.nom + "</li>" );
+		$(".cabecera").append("<tr><td>" + data.dorsal + "</td><td>" + data.nom + "</td><td>"+ data.punts
+ + "</td></tr>")
 	})
 
-
-})
+});
